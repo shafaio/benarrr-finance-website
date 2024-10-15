8a9e435e89677b9df0085202c77cf46570a06cce
Shafa Asyari Ramadhan <shafa.asyari.02@gmail.com>
2024-10-16 00:44:34 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          DEFAULT: "1130px",
+        },
+      },
+      colors: {
+        primary: "#5313CA",
+        secondary: "#FF9736",
+        tertiary: "#75F1F1",
+        black: "#050729",
+      },
+      fontFamily: {
+        clash: ["Clash Display"],
+      },
+      keyframes: {
+        "infinite-scroll": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        "infinite-scroll": "infinite-scroll 2s linear infinite",
+      },
+      boxShadow: {
+        custom: "0 0 50px -2px rgba(5, 7, 41, 0.11)",
+      },
+    },
+  },
+  plugins: [],
+};
